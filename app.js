1993ec6151f82e4b82acd432d91cee7c572c62c4
KatieAJenkins <kind2karma@gmail.com>
2017-02-28 16:03:40 -0700

    added public folder with index.html. Launched program

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 'use strict';
 
 var five = require('johnny-five');
+var express = require('express');
+var app = express();
 var myBoard;
 
 myBoard = new five.Board();
 
+app.use(express.static(__dirname + '/public'));
+
 myBoard.on("ready" , function() {
   console.log("Arduino is ready!");
+});
+
+app.listen('3000', function (){
+  console.log("Listening on port ", 3000);
 });
