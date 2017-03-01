b03ea876769ed5d600e0b095430871f0752529a6
KatieAJenkins <kind2karma@gmail.com>
2017-03-01 15:26:19 -0700

    cleaned up code

--- app.js
+++ app.js
@@ -32,24 +32,16 @@ myBoard.on("ready" , function() {
   let buzzer1 = new five.Led(3);
   let buzzer2 = new five.Led(6);
 
-    //controls length of time for buzzers to pulse
-    // buzzer1.pulse(1000);
-    // buzzer2.pulse(1000);
-
-    //first argument controls time for buzzers to pulse
-    // this.wait(10000, function() {
-    //   buzzer1.stop().off();
-    //   buzzer2.stop().off();
-    // });
-
     function buzzerOn(){
       //buzzer1
       myBoard.wait(null, function() {
+        //pulse for 1 second
         // buzzer1.pulse(1000);
       });
 
       //buzzer2
       myBoard.wait(1000, function() {
+        //pulse for 1 second
         // buzzer2.pulse(1000);
       });
 
@@ -73,61 +65,6 @@ myBoard.on("ready" , function() {
     //invoke buzzerOff() function
     buzzerOff();
   });
-
-//
-//   //initial state for buzzers
-//   let state = {
-//     buzzer1: 0, //on: 100;
-//     buzzer2: 0 //off: 0;
-//   };
-//   console.log(state);
-//
-//   //map pins to analog output
-//   buzzers = new five.Board ({
-//     pins: {
-//       buzzer1: 3,
-//       buzzer2: 6
-//     }
-//   });
-//
-//   //helper function to set state
-//   let setBuzzState = function(state){
-//     buzzers.buzzValue({
-//       buzzer1: state.buzzer1, //on: state.on,
-//       buzzer2: state.buzzer2 //off: state
-//     });
-//   };
-//
-//
-// // Listen to the web socket connection
-//
-//   io.on('connection', function(client) {
-//     client.on('join', function(handshake){
-//       console.log(handshake);
-//     });
-//
-//     //set initial state
-//     setBuzzState(state);
-//     console.log(state);
-//
-//     //every time an event is sent, listen to it & grab new value for each individual buzzer
-//
-//     client.on('eventName' , function(data) {
-//       state.buzzer1 = data.buzzValue === 100 ? data.value : state.buzzer1;
-//       state.buzzer2 = data.buzzValue === 100 ? data.value : state.buzzer2;
-//
-//       //set new buzz value
-//       setBuzzState(state);
-//
-//       //socket Listening
-//       client.emit('eventName', data);
-//       client.broadcast.emit('eventName', data);
-//     });
-//
-//     //turn on the buzzers
-//     buzzers.on();
-//   }); //end of io.on sockets
-// }); //end of myBoard.on
 
 //express server listening on port
 app.listen(port, function (){
