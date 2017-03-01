6004f5b037186ed0d0adc1b80c039a22f7a7e9c4
KatieAJenkins <kind2karma@gmail.com>
2017-03-01 12:08:44 -0700

    completed app.js and client.js to test intial buzzer funcationality

---
+++ client.js
@@ -0,0 +1,27 @@
+'use strict';
+
+(function() {
+  console.log("connected to client.js");
+    var socket = io.connect(window.location.hostname + ':' + 3000);
+    var buzzer1 = document.getElementById('buzzer1');
+    var buzzer2 = document.getElementById('buzzer2');
+
+    function emitValue(buzzValue, e) {
+        socket.emit('eventName', {
+            buzzValue: buzzValue,
+            value: e.target.value
+        });
+    }
+
+    buzzer1.addEventListener('change', emitValue.bind(null, 100));
+    buzzer2.addEventListener('change', emitValue.bind(null, 100));
+
+    socket.on('connect', function(data) {
+        socket.emit('join', 'Client is connected!');
+    });
+
+    socket.on('eventName', function(data) {
+        var buzzValue = data.buzzValue;
+        document.getElementById(buzzValue).value = data.value;
+    });
+}());
