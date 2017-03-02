5540091eeea4447ac3539823d01c2bdaf0f3f2cf
KatieAJenkins <kind2karma@gmail.com>
2017-03-01 19:51:39 -0700

    cleaned up code

--- app.js
+++ app.js
@@ -1,8 +1,5 @@
 'use strict';
 
-//nwjs process.stdin fix to use with johnny-five
-// nw.require("nwjs-j5-fix").fix();
-
 //express server setup
 const express = require('express');
 const app = express();
@@ -12,59 +9,53 @@ var port = process.env.PORT || 3000;
 const five = require('johnny-five');
 var myBoard = new five.Board();
 
-//sockets testing
-// const server = require('http').Server(app);
-// const io = require('socket.io')(server);
-
-let buzzers = null;
-
 app.use(express.static(__dirname + '/public'));
-app.get('/', function(req, res, next) {
-  res.sendFile(__dirname + '/index.html')
-});
 
-myBoard.on("ready" , function() {
-  console.log("Arduino is ready!");
-  // led initial test
-  // var led = new five.Led(13);
-  //   led.blink(500);
 
-  let buzzer1 = new five.Led(3);
-  let buzzer2 = new five.Led(6);
 
-    function buzzerOn(){
-      //buzzer1
-      myBoard.wait(null, function() {
-        //pulse for 1 second
-        // buzzer1.pulse(1000);
-      });
+  myBoard.on("ready" , function() {
+    console.log("Arduino is ready!");
+    // led initial test
+    // var led = new five.Led(13);
+    //   led.blink(500);
 
-      //buzzer2
-      myBoard.wait(1000, function() {
-        //pulse for 1 second
-        // buzzer2.pulse(1000);
-      });
+    let buzzer1 = new five.Led(3);
+    let buzzer2 = new five.Led(6);
 
-      //turn off buzzer after 10 seconds
-      myBoard.wait(10000, function() {
-        buzzer1.stop().off();
-        buzzer2.stop().off();
-      });
-    }
+      function buzzerOn(){
+        //buzzer1
+        myBoard.wait(null, function() {
+          // pulse for 1 second
+          // buzzer1.pulse(1000);
+        });
 
-    //invoke buzzerOn() function
-    buzzerOn();
+        //buzzer2
+        myBoard.wait(1000, function() {
+          //pulse for 1 second
+          // buzzer2.pulse(1000);
+        });
 
-    function buzzerOff(){
-      myBoard.wait(10000, function(){
-        buzzer1.stop().off();
-        buzzer2.stop().off();
-      });
-    }
+        //turn off buzzer after 10 seconds
+        myBoard.wait(10000, function() {
+          buzzer1.stop().off();
+          buzzer2.stop().off();
+        });
+      }
 
-    //invoke buzzerOff() function
-    buzzerOff();
-  });
+      //turn on Buzzer
+      buzzerOn();
+
+      function buzzerOff(){
+        myBoard.wait(10000, function(){
+          buzzer1.stop().off();
+          buzzer2.stop().off();
+        });
+      }
+
+      //turn off Buzzer
+      buzzerOff();
+
+    });//end of myBoard
 
 //express server listening on port
 app.listen(port, function (){
--- public/js/client.js
+++ public/js/client.js
@@ -15,27 +15,5 @@
     off.addEventListener("click" , function() {
       console.log("You clicked off!");
     });
-    // console.log(buzzer1.addEventListener('change', emitValue.bind(null, 100)));
-    // buzzer2.addEventListener('change', emitValue.bind(null, 100));
 
-    //testing with sockets below
-
-    // function emitValue(buzzValue, e) {
-    //     socket.emit('eventName', {
-    //         buzzValue: buzzValue,
-    //         value: e.target.value
-    //     });
-    // }
-
-    // buzzer1.addEventListener('change', emitValue.bind(null, 100));
-    // buzzer2.addEventListener('change', emitValue.bind(null, 100));
-    //
-    // socket.on('connect', function(data) {
-    //     socket.emit('join', 'Client is connected!');
-    // });
-
-    // socket.on('eventName', function(data) {
-    //     var buzzValue = data.buzzValue;
-    //     document.getElementById(buzzValue).value = data.value;
-    // });
 }());
