110a6d74ec0f15628b65f4fe52a5772bfbdbcea4
KatieAJenkins <kind2karma@gmail.com>
2017-03-02 13:16:58 -0700

    removed testing code

--- public/js/client.js
+++ public/js/client.js
@@ -41,30 +41,3 @@ button.addEventListener('click', function(e) {
   });
 
 });
-//
-// ////////TESTING///////
-// // (function() {
-// //   console.log("document ready!");
-// //     var on = document.getElementById("on");
-// //     console.log(on);
-// //
-// //     var off = document.getElementById("off");
-// //     console.log(off);
-// //
-// //     on.addEventListener("click" , function() {
-// //       console.log("You clicked on!");
-// //       $.ajax({
-// //         url: '/app.js',
-// //         type: 'GET',
-// //         success: function(res){
-// //           console.log(res);
-// //           // alert(res);
-// //         }
-// //       });
-// //     });
-// //
-// //     off.addEventListener("click" , function() {
-// //       console.log("You clicked off!");
-// //     });
-// //
-// // }());
