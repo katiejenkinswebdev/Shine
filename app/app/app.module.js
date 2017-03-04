f4903554930a03774f986fc9314f2c0f20c996d7
KatieAJenkins <kind2karma@gmail.com>
2017-03-04 16:52:53 -0700

    continued work setting up angular material

--- app/app/app.module.js
+++
@@ -1,8 +0,0 @@
-(function() {
-  'use strict';
-  angular
-    .module("app"  , ['ngMaterial'])
-    .module('MyApp', ['ngMaterial'])
-      .run(function() {
-      console.log("My App is ready!")
-}());
