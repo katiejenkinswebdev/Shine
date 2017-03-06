78565fbed660703321b630ef7c949f1d6afe24c8
KatieAJenkins <kind2karma@gmail.com>
2017-03-06 11:16:08 -0700

    ui router hooked up correctly with states displaying on icon clicks

--- public/app/app.config.js
+++ public/app/app.config.js
@@ -4,54 +4,54 @@
 
  var routerApp = angular
     .module('app', [ 'ui.router', 'control.component','learn.component' , 'profile.component'])
-    // .config(config);
+    .config(config);
 
-  // config.$inject = ['$stateProvider' , '$urlRouterProvider', '$locationProvider'];
+  config.$inject = ['$stateProvider' , '$urlRouterProvider', '$locationProvider'];
 
-  routerApp.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+  // routerApp.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
 
-    $urlRouterProvider.otherwise('/');
+    // $urlRouterProvider.otherwise('/');
+
+    // $locationProvider.html5Mode(true);
+  //
+  //   $stateProvider
+  //   //HOME STATES AND NESTED VIEWS
+  //     .state( 'control', {
+  //       url: '/control',
+  //       // templateUrl: 'control.template.html',
+  //       component: 'control'
+  //     })
+  //     .state( 'learn' , {
+  //       url: '/learn',
+  //       // templateUrl: 'learn.template.html',
+  //       component: 'learn'
+  //     })
+  //     .state( 'profile' , {
+  //       url: '/profile',
+  //       // templateUrl: 'learn.template.html',
+  //       component: 'profile'
+  //     });
+  // });
+
+  function config($stateProvider, $urlRouterProvider, $locationProvider){
 
     $locationProvider.html5Mode(true);
 
     $stateProvider
-    //HOME STATES AND NESTED VIEWS
-      .state( 'control', {
-        url: '/control',
-        // templateUrl: 'control.template.html',
-        component: 'control'
+      .state({
+        name: 'control',
+        url: '/',
+        component: 'control',
       })
-      .state( 'learn' , {
+      .state({
+        name: 'learn',
         url: '/learn',
-        // templateUrl: 'learn.template.html',
         component: 'learn'
       })
-      .state( 'profile' , {
+      .state({
+        name: 'profile',
         url: '/profile',
-        // templateUrl: 'learn.template.html',
         component: 'profile'
       });
-  });
-
-  // function config($stateProvider, $urlRouterProvider, $locationProvider){
-
-    // $locationProvider.html5Mode(true);
-
-    // $stateProvider
-    //   .state({
-    //     name: 'control',
-    //     url: '/',
-    //     component: 'control',
-    //   })
-    //   .state({
-    //     name: 'learn',
-    //     url: '/:learn',
-    //     component: 'learn'
-    //   })
-    //   .state({
-    //     name: 'profile',
-    //     url: '/:profile',
-    //     component: 'profile'
-    //   });
-    // }
+    }
 }());
