2f00bf5eb5043f932cd6b6567ba8f87d97cd03e6
KatieAJenkins <kind2karma@gmail.com>
2017-03-05 19:33:38 -0700

    buzzers turning on through button

--- public/controls/control.component.js
+++ public/controls/control.component.js
@@ -17,7 +17,48 @@ angular
     vm.$onInit = onInit;
     vm.submitNewTreatment = submitNewTreatment;
     vm.treatments = [];
-    // vm.countdown = countdown;
+
+////previous working code//////////
+
+    // var pubnub = PUBNUB.init({
+    //   publish_key: 'pub-c-c38b69e7-3a86-4037-939b-98aa303bd887',
+    //   subscribe_key: 'sub-c-45239d26-ff7d-11e6-8ce0-0619f8945a4f',
+    //   ssl:
+    //   true,
+    // });
+
+    // var channel = 'buzzers';
+    //
+    // var button = document.querySelector('button');
+    //
+    // var buzzState = true;
+    //
+    // // Subscribe data from all subscribers of the channel to set the button state correctly
+    //
+    // pubnub.subscribe({
+    //   channel: channel,
+    //   message: function(message) {
+    //     buzzState = message.buzz; // raw data
+    //     buzzState = !buzzState; // toggle to label button
+    //     button.textContent = (buzzState) ? 'Buzzers On' : 'Stop Buzzers';
+    //     console.log(buzzState);
+    //   }
+    // });
+    //
+    // // Upon a button click, publish the data. Arduino will subscribe it and turn on buzzers
+    //
+    // button.addEventListener('click', function(e) {
+    //   pubnub.publish({
+    //     channel: channel,
+    //     message: {buzz: buzzState},
+    //     callback: function(message) {
+    //       console.log(message);
+    //     }
+    //   });
+    // });
+
+      }//end of Controller
+
 
     function onInit(){
       console.log("we made it to Control Component onInit");
@@ -31,24 +72,56 @@ angular
     function submitNewTreatment (seconds, rating) {
       console.log("submitNewTreatment triggered!");
 
-      var treatment = {seconds:seconds, rating:rating};
-      console.log('treatment ' ,treatment);
 
-      $http.post('/api/treatments', treatment)
-        .then(response => {
-          console.log(response.data);
-          // vm.treatments.push(treatment);
-          // delete vm.treatment;
+      let pubnub = PUBNUB.init({
+        publish_key: 'pub-c-c38b69e7-3a86-4037-939b-98aa303bd887',
+        subscribe_key: 'sub-c-45239d26-ff7d-11e6-8ce0-0619f8945a4f',
+        ssl:
+        true,
+      });
+
+      let channel = 'buzzers';
+
+      var button = document.querySelector('button');
+      console.log(button);
+
+      let buzzState = true;
+      console.log(buzzState);
+
+      // // Subscribe data from all subscribers of the channel to set the button state correctly
+      //
+      pubnub.subscribe({
+        channel: channel,
+        message: function(message) {
+          buzzState = message.buzz; // raw data
+          buzzState = !buzzState; // toggle to label button
+          button.textContent = (buzzState) ? 'Buzzers On' : 'Stop Buzzers';
+          console.log(buzzState);
+        }
+      });
+
+        pubnub.publish({
+          channel: channel,
+          message: {buzz: buzzState},
+          callback: function(message) {
+            console.log(message);
+          }
         });
+
+      // var treatment = {seconds:seconds, rating:rating};
+      // console.log('treatment ' ,treatment);
+      //
+      // $http.post('/api/treatments', treatment)
+      //   .then(response => {
+      //     console.log(response.data);
+      //     // vm.treatments.push(treatment);
+      //     // delete vm.treatment;
+      //   });
     }
 
-    // function countdown (seconds) {
-    //   while(seconds > 0){
-    //   console.log(seconds);
-    // }
-    // }
 
-
+//declare variables at top
+//move code into button to submit post
 
 //TODO translate into angular
 // var pubnub = PUBNUB.init({
@@ -88,5 +161,5 @@ angular
 //   });
 // });
 
-  }//end of Controller
-}());
+
+}()); //end of Controller
