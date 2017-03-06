de1cb566ea5f983521a17ea331d974b92d0e2f70
KatieAJenkins <kind2karma@gmail.com>
2017-03-06 11:21:50 -0700

    cleaned up ui router state code

--- public/app/app.config.js
+++ public/app/app.config.js
@@ -2,36 +2,11 @@
 'use strict';
 // console.log('app.config connected!');
 
- var routerApp = angular
+  angular
     .module('app', [ 'ui.router', 'control.component','learn.component' , 'profile.component'])
     .config(config);
 
   config.$inject = ['$stateProvider' , '$urlRouterProvider', '$locationProvider'];
-
-  // routerApp.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
-
-    // $urlRouterProvider.otherwise('/');
-
-    // $locationProvider.html5Mode(true);
-  //
-  //   $stateProvider
-  //   //HOME STATES AND NESTED VIEWS
-  //     .state( 'control', {
-  //       url: '/control',
-  //       // templateUrl: 'control.template.html',
-  //       component: 'control'
-  //     })
-  //     .state( 'learn' , {
-  //       url: '/learn',
-  //       // templateUrl: 'learn.template.html',
-  //       component: 'learn'
-  //     })
-  //     .state( 'profile' , {
-  //       url: '/profile',
-  //       // templateUrl: 'learn.template.html',
-  //       component: 'profile'
-  //     });
-  // });
 
   function config($stateProvider, $urlRouterProvider, $locationProvider){
 
